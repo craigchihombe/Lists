b530d66b517bc33762157402b4a68fb8854631be
craigchihombe <achihombe@gmail.com>
2024-09-15 10:31:50 +0200

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,16 @@
+#create an empty list
+empty_list = []
+print(empty_list)
+
+#list of numbers
+my_list = [1 ,2 ,3 ,4 ,5]
+print(my_list)
+
+#use * operator
+my_list2 = [6, 7, 8] * 3
+print(my_list2)
+
+#reverse the given list
+my_list3 = [934, 106, 423, 298, 982]
+my_list3 = my_list3[::-1]
+print(my_list3)
